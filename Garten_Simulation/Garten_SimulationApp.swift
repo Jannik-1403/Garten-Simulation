c8bbeb99a7772da6670a7614f97f793d5838d557
Jannik <jannik.schill.2010@gmail.com>
2026-03-21 10:49:57 +0100

    Initial Commit

---
+++ Garten_Simulation/Garten_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Garten_SimulationApp.swift
+//  Garten_Simulation
+//
+//  Created by Jannik Schill on 21.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Garten_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Garten_SimulationTests/Garten_SimulationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Garten_SimulationTests.swift
+//  Garten_SimulationTests
+//
+//  Created by Jannik Schill on 21.03.26.
+//
+
+import Testing
+@testable import Garten_Simulation
+
+struct Garten_SimulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
