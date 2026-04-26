15d7a0eed52b1839d2564ec5b8ae785637703fcb
Jannik <jannik.schill.2010@gmail.com>
2026-04-26 12:55:41 +0200

    small improvement

---
+++ GartenWidget/GartenWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GartenWidgetBundle.swift
+//  GartenWidget
+//
+//  Created by Jannik Schill on 25.04.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GartenWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GartenWidget()
+    }
+}
