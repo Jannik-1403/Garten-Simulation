e98fdc389ebc1ef181da88e29ce9c727673ce5af
Jannik <jannik.schill.2010@gmail.com>
2026-07-31 10:49:08 +0200

    Fix: Revert Screen Time auth back to .individual because .child fails on adult iCloud accounts

--- Garten_Simulation/Managers/ScreenTimeManager.swift
+++ Garten_Simulation/Managers/ScreenTimeManager.swift
@@ -228,7 +228,7 @@ class ScreenTimeManager: ObservableObject {
     
     func requestAuthorization() async throws {
         do {
-            try await AuthorizationCenter.shared.requestAuthorization(for: .child)
+            try await AuthorizationCenter.shared.requestAuthorization(for: .individual)
             DispatchQueue.main.async {
                 self.checkAuthorizationStatus()
             }
